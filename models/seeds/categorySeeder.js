e93857f0238884d92dd80d009adb0a9d1ef977fe
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-20 20:20:43 +0800

    feat: add recordSeeders

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -5,14 +5,11 @@ const category = require('../../config/category.json')
 
 db.once('open', () => {
   for (let key in category) {
-    // console.log(category[key])
-    // console.log(category[key].categoryCH)
     Category.create({
       categoryEN: key,
       categoryCH: category[key].categoryCH,
       iconHTML: category[key].iconHTML
     })
   }
-
   console.log('done!')
 })
