3df67ec5d5d8c2bc086457c0030bd0f37e71ce66
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-20 22:31:14 +0800

    feat: set home route & send data to index

--- app.js
+++ app.js
@@ -13,8 +13,40 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const Record = require('./models/record')
+const Category = require('./models/category')
+let category = ''
+
 app.get('/', (req, res) => {
-  res.render('index')
+  Promise.all([Record.find().lean(), Category.find().lean()])
+    .then(results => {
+      const records = results[0]
+      const category = results[1]
+      records.forEach(record => {
+        const categoryFound = category.find(category =>
+          category.categoryEN === record.category
+        )
+        record.category = categoryFound.iconHTML
+      })
+      res.render('index', { records })
+    })
+    .catch(error => console.error(error))
+
+  // Record.find()
+  //   .lean()
+  //   .then(record => {
+  //     Category.find({ categoryEN: record[0].category })
+  //       .lean()
+  //       .then(category => {
+  //         category = category[0].iconHTML
+  //         // console.log(category)
+  //         // res.render('index', { category: category[0] })
+  //       })
+  //       .catch(error => console.log(error))
+  //     console.log(category)
+  //     res.render('index', { record: record[0], category })
+  //   })
+
 })
 
 app.get('/records/new', (req, res) => {
@@ -22,16 +54,16 @@ app.get('/records/new', (req, res) => {
 })
 
 ///修改紀錄
-app.get('/records/:id/edit',(req,res)=>{
+app.get('/records/:id/edit', (req, res) => {
   res.render('edit')
 })
-app.put('/rcords/:id',(req,res)=>{
+app.put('/rcords/:id', (req, res) => {
 
 })
 
 ////刪除紀錄
-app.delete('/records/:id',(req,res)=>{
-  
+app.delete('/records/:id', (req, res) => {
+
 })
 
 
