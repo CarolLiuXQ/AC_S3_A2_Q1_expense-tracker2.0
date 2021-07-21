d5f5c10c64ff015a06b9586c1df098c074fe99cc
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-21 20:24:23 +0800

    refactor: router & home & index & records

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
+const routes = require('./routes/index')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -15,73 +16,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-const Record = require('./models/record')
-const Category = require('./models/category')
-
-app.get('/', (req, res) => {
-  Promise.all([Record.find().lean(), Category.find().lean()])
-    .then(results => {
-      const records = results[0]
-      const category = results[1]
-      records.forEach(record => {
-        const categoryFound = category.find(category =>
-          category.categoryEN === record.category
-        )
-        record.category = categoryFound.iconHTML
-      })
-      res.render('index', { records })
-    })
-    .catch(error => console.error(error))
-})
-
-///new page 
-app.get('/records/new', (req, res) => {
-  res.render('new')
-})
-///新增紀錄
-app.post('/', (req, res) => {
-  const { name, date, category, amount } = req.body
-  return Record.create({ name, date, category, amount })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-
-////刪除紀錄
-app.delete('/records/:id', (req, res) => {
-  const id = req.params.id
-  Record.findById(id)
-    .then(record => record.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-
-///修改紀錄
-app.get('/records/:id/edit', (req, res) => {
-  Record.findById(req.params.id)
-    .lean()
-    .then(record => res.render('edit', { record }))
-    .catch(error => console.log(error))
-})
-
-app.put('/records/:id', (req, res) => {
-  const { name, date, category, amount } = req.body
-  const id = String(req.params.id)
-  return Record.findById(id)
-    .then(record => {
-      record.name = name
-      record.date = date
-      record.category = category
-      record.amount = amount
-      return record.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-
+app.use(routes)
 
 
 app.listen(PORT, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/', (req, res) => {
+  Promise.all([Record.find().lean(), Category.find().lean()])
+    .then(results => {
+      const records = results[0]
+      const category = results[1]
+      records.forEach(record => {
+        const categoryFound = category.find(category =>
+          category.categoryEN === record.category
+        )
+        record.category = categoryFound.iconHTML
+      })
+      res.render('index', { records })
+    })
+    .catch(error => console.error(error))
+})
+
+///新增紀錄
+router.post('/', (req, res) => {
+  const { name, date, category, amount } = req.body
+  return Record.create({ name, date, category, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
