5ae0b51478326e52c94300abad896a6f1e9b7868
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-23 23:51:03 +0800

    DB test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars')
 const routes = require('./routes/index')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 require('./config/mongoose')
 
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const db = mongoose.connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+const MONGODB_URI = 'mongodb+srv://root:21RbD0uTJpWvysGZ@cluster0.avghh.mongodb.net/expense-tracker?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
   , useUnifiedTopology: true
