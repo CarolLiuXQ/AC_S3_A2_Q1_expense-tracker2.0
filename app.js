dcad54afd213599c8e0cf19d29fae70406972f8e
CarolLiuXQ <s7910565@gmail.com>
2021-10-29 00:30:35 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const routes = require('./routes/index')
+const session = require('express-session')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -15,6 +16,13 @@ app.engine('handlebars', exphbs({
   helpers: require('./controller/handlebarsHelper')
 }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true,
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
